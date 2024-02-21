cde4fcd80f5d0e90bc80800ff5301c7c83665ef1
Roger Dubar <rdubar@gmail.com>
2024-02-21 22:32:11 +0000

    Add minidec

--- rogkit_package/bin/cat_age.py
+++ rogkit_package/bin/cat_age.py
@@ -1,4 +1,5 @@
 import sys
+from .minidec import minimize_decimals
 
 def cat_age_to_human(cat_age_years, cat_age_months=0):
     total_cat_age_months = cat_age_years * 12 + cat_age_months
@@ -25,7 +26,13 @@ def main():
     cat_age_months = float(sys.argv[2]) if len(sys.argv) > 2 else 0
 
     human_years = cat_age_to_human(cat_age_years, cat_age_months)
-    print(f"A cat that is {cat_age_years} years and {cat_age_months} months old is approximately {human_years:.2f} human years old.")
+
+    cat = f'{minimize_decimals(cat_age_years, 0)} years' if cat_age_years else ''
+    human = minimize_decimals(human_years, 1)
+    months = f' and {minimize_decimals(cat_age_months, 0)} months' if cat_age_months else ''
+    _and = ' and ' if cat_age_years and cat_age_months else ''
+
+    print(f"A cat that is{cat}{_and}{months} old is approximately {human} human years old.")
 
 if __name__ == "__main__":
     main()
---
+++ rogkit_package/bin/minidec.py
@@ -0,0 +1,22 @@
+import sys
+
+def minimize_decimals(value, max_decimals):
+    if value == int(value):
+        return str(int(value))
+    else:
+        format_string = "{:." + str(max_decimals) + "f}"
+        formatted_value = format_string.format(value)
+        while formatted_value[-1] == "0":
+            formatted_value = formatted_value[:-1]
+        return formatted_value
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: minidec <value> <max_decimals>\nMinimize the number of decimals in a number.")
+        sys.exit(1)
+    
+    value = float(sys.argv[1])
+    max_decimals = int(sys.argv[2]) if len(sys.argv) > 2 else 2
+
+    print(minimize_decimals(value, max_decimals))
