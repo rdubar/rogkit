7ca5f2a63f48e2ec3eed83a6202048127e88eb9f
Roger Dubar <rdubar@gmail.com>
2024-05-29 14:46:43 +0100

    venv path improvements

--- rogkit_package/bin/venv_set.py
+++ rogkit_package/bin/venv_set.py
@@ -5,12 +5,11 @@ def find_virtual_envs(directory):
     return [folder for folder in os.listdir(directory) if os.path.exists(os.path.join(directory, folder, 'bin', 'activate'))]
 
 def activate_virtual_env(directory):
-    for root, dirs, files in os.walk(directory):
-        if 'bin' in dirs and 'activate' in os.listdir(os.path.join(root, 'bin')):
-            activate_script = os.path.join(root, 'bin', 'activate')
-            command = f"source {activate_script}"
-            subprocess.run(command, shell=True, executable="/bin/bash")
-            return activate_script
+    activate_script = os.path.join(directory, 'bin', 'activate')
+    if os.path.exists(activate_script):
+        command = f"source {activate_script}"
+        subprocess.run(command, shell=True, executable="/bin/bash")
+        return activate_script
     return None
 
 def main():
@@ -18,14 +17,15 @@ def main():
     env_folders = find_virtual_envs(current_directory)
     
     print("Rog's Virtual Environment Tool")
-    v_list = '' if len(env_folders)==0 else ': ' + str(env_folders)
-    folders = 'folder' if len(env_folders)==1 else 'folders'
+    v_list = '' if len(env_folders) == 0 else ': ' + ', '.join(env_folders)
+    folders = 'folder' if len(env_folders) == 1 else 'folders'
     print(f'Found {len(env_folders)} virtual environment {folders} in {current_directory}{v_list}')
     
     if env_folders:
         first_env = env_folders[0]
-        print(f"Activating the first virtual environment found in {first_env}")
-        activate_script = activate_virtual_env(first_env)
+        full_path = os.path.join(current_directory, first_env)
+        print(f"Activating the first virtual environment found in {first_env} at {full_path}")
+        activate_script = activate_virtual_env(full_path)
         if activate_script:
             print(f"Activated {activate_script}")
         else:
