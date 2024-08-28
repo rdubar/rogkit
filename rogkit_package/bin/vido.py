4fe29806b861be7ed4f333039124ccca4ffff53f
Roger Dubar <rdubar@gmail.com>
2024-08-28 13:36:39 +0100

    vido: fix long filenames

--- rogkit_package/bin/vido.py
+++ rogkit_package/bin/vido.py
@@ -66,17 +66,27 @@ def process_url(url, config):
     title = get_title_from_url(url)
     if not title:
         return
+
     print(Fore.CYAN + "Downloading:", title)
     try:
         with YoutubeDL(config.get_download_options()) as ydl:
             video_info = ydl.extract_info(url, download=True)
             output = ydl.prepare_filename(video_info)
+            
+            # Truncate the output filename if it's too long
+            max_length = 100  # Adjust this value as needed
+            base, ext = os.path.splitext(output)
+            if len(base) > max_length:
+                base = base[:max_length]
+                output = f"{base}{ext}"
+            
+            final_output = os.path.join(config.download_folder, output)
+            os.rename(ydl.prepare_filename(video_info), final_output)
+
     except Exception as e:
         print(Fore.MAGENTA + f"Error downloading {url}\n{e}")
         return
 
-    final_output = os.path.join(config.download_folder, output)
-    os.rename(output, final_output)
     print(Fore.GREEN + f"Downloaded to {final_output}")
 
 def get_movies(search, config):
